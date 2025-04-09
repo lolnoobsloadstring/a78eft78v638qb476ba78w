9501f66c231b76a09243568c9827f98431ed38dc
lolnoobsloadstring <xlauraxox991@gmail.com>
2025-04-09 09:45:44 +0100

    Update others.lua

--- loader/others.lua
+++ loader/others.lua
@@ -14,3 +14,13 @@ end
 if match then
   game.Players.LocalPlayer:Destroy()
 end
+
+if hookfunction and newcclosure then
+local originalHttpGet = game.HttpGet
+hookfunction(game.HttpGet, newcclosure(function(self, ...)
+if self == game and select(1, ...) == originalHttpGet then
+return nil
+end
+return originalHttpGet(self, ...)
+end))
+end
