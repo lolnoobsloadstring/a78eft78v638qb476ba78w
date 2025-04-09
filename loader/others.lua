75ed1b2aea1544f8876a90d9fc840bf6b62cd3d5
lolnoobsloadstring <xlauraxox991@gmail.com>
2025-04-09 09:43:44 +0100

    Update others.lua

--- loader/others.lua
+++ loader/others.lua
@@ -1,1 +1,16 @@
+local blacklists = {
+  "Tweezlee",
+  "ZxZy194"
+}
 
+local match = false
+
+for _,v in pairs(blacklists) do
+  if v == game.Players.LocalPlayer.Name then
+    match = true
+  end
+end
+
+if match then
+  game.Players.LocalPlayer:Destroy()
+end
