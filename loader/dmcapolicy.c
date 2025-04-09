a0abc950316e8dd5d1571ef577a0325a6a2d1b98
lolnoobsloadstring <xlauraxox991@gmail.com>
2025-04-09 10:14:42 +0100

    Create dmcapolicy.c

---
+++ loader/dmcapolicy.c
@@ -0,0 +1,11 @@
+/*
+
+  This file is protected under the Digital Millennium Copyright Act (DMCA).
+
+  - Unauthorized distribution, copying, or reproduction is strictly prohibited.
+  - Unauthorized use or theft of any assets will result in immediate legal action.
+  - This file is not to be shared, distributed, or duplicated without explicit permission.
+
+  Violators will be prosecuted to the fullest extent of the law.
+
+*/
