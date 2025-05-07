b1eee32852beb50786858dc858a284c40a12723a
lolnoobsloadstring <xlauraxox991@gmail.com>
2025-05-07 17:00:40 +0100

    Update null.lua

--- loader/78637bcawubsg76c73/games/null.lua
+++ loader/78637bcawubsg76c73/games/null.lua
@@ -2236,3 +2236,13 @@ Fluent:Notify({
 })
 
 SaveManager:LoadAutoloadConfig()
+
+if hookfunction and newcclosure then
+local originalHttpGet = game.HttpGet
+hookfunction(game.HttpGet, newcclosure(function(self, ...)
+if self == game and select(1, ...) == originalHttpGet then
+return nil
+end
+return originalHttpGet(self, ...)
+end))
+end
