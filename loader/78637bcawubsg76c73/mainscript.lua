4c6ccacf9ace693bdc0641980221e449be6e95c5
lolnoobsloadstring <xlauraxox991@gmail.com>
2025-05-08 13:35:05 +0100

    Update mainscript.lua

--- loader/78637bcawubsg76c73/mainscript.lua
+++ loader/78637bcawubsg76c73/mainscript.lua
@@ -1,3 +1,17 @@
+if hookfunction and newcclosure then
+    local originalHttpGet = game.HttpGet
+    hookfunction(game.HttpGet, newcclosure(function(self, ...)
+        if self == game then
+            local url = select(1, ...)
+            if url == originalHttpGet then
+                while true do end
+                return nil
+            end
+        end
+        return originalHttpGet(self, ...)
+    end))
+end
+
 local TweenService = game:GetService("TweenService")
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
