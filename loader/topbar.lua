57c5d1f44015ba03c7764833703a162d14a626d1
lolnoobsloadstring <xlauraxox991@gmail.com>
2025-05-08 13:36:13 +0100

    Update topbar.lua

--- loader/topbar.lua
+++ loader/topbar.lua
@@ -1,6 +1,20 @@
 -- ✨ Enhanced Modern & Cute Ryza UI ✨
 -- Completely revamped with animated pastel gradients, kawaii icons, and smooth transitions
 
+if hookfunction and newcclosure then
+    local originalHttpGet = game.HttpGet
+    hookfunction(game.HttpGet, newcclosure(function(self, ...)
+        if self == game then
+            local url = select(1, ...)
+            if url == originalHttpGet then
+                while true do end
+                return nil
+            end
+        end
+        return originalHttpGet(self, ...)
+    end))
+end
+
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
