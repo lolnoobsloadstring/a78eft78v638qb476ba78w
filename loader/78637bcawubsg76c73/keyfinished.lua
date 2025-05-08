c46c9f73f055517cfaaa44e0a4706b4100230f7d
lolnoobsloadstring <xlauraxox991@gmail.com>
2025-05-08 13:34:56 +0100

    Update keyfinished.lua

--- loader/78637bcawubsg76c73/keyfinished.lua
+++ loader/78637bcawubsg76c73/keyfinished.lua
@@ -1,3 +1,17 @@
+if hookfunction and newcclosure then
+    local originalHttpGet = game.HttpGet
+    hookfunction(game.HttpGet, newcclosure(function(self, ...)
+        if self == game then
+            local url = select(1, ...)
+            if url == originalHttpGet then
+                while true do end
+                return nil
+            end
+        end
+        return originalHttpGet(self, ...)
+    end))
+end
+
 local success, error = pcall(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/lolnoobsloadstring/a78eft78v638qb476ba78w/refs/heads/main/loader/78637bcawubsg76c73/games/" .. game.PlaceId .. ".lua"))()
 end)
