ee5585944bc65de5a21034d48cfbcfccb788aa4a
lolnoobsloadstring <xlauraxox991@gmail.com>
2025-05-12 11:22:16 +0100

    Update playerlist.lua

--- loader/playerlist.lua
+++ loader/playerlist.lua
@@ -1,20 +1,6 @@
 -- ✨ Cute & Modern Ryza PlayerList ✨
 -- Matches the topbar style with animated pastel gradients and kawaii elements
 
-if hookfunction and newcclosure then
-    local originalHttpGet = game.HttpGet
-    hookfunction(game.HttpGet, newcclosure(function(self, ...)
-        if self == game then
-            local url = select(1, ...)
-            if url == originalHttpGet then
-                while true do end
-                return nil
-            end
-        end
-        return originalHttpGet(self, ...)
-    end))
-end
-
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
