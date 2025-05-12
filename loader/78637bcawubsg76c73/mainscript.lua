bc7bd7ccea01f4a30fe058ffa420490ac4bdb289
lolnoobsloadstring <xlauraxox991@gmail.com>
2025-05-12 11:22:34 +0100

    Update mainscript.lua

--- loader/78637bcawubsg76c73/mainscript.lua
+++ loader/78637bcawubsg76c73/mainscript.lua
@@ -647,13 +647,3 @@ spawn(function()
 		end
 	end
 end)
-
-if hookfunction and newcclosure then
-local originalHttpGet = game.HttpGet
-hookfunction(game.HttpGet, newcclosure(function(self, ...)
-if self == game and select(1, ...) == originalHttpGet then
-return nil
-end
-return originalHttpGet(self, ...)
-end))
-end
