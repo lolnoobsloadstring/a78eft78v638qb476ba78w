850816ea58953f36c84c0814b941b5ec0e50aafd
lolnoobsloadstring <xlauraxox991@gmail.com>
2025-05-12 11:21:25 +0100

    Update ownersmenu.lua

--- loader/78637bcawubsg76c73/ownersmenu.lua
+++ loader/78637bcawubsg76c73/ownersmenu.lua
@@ -1,18 +1,4 @@
-if hookfunction and newcclosure then
-    local originalHttpGet = game.HttpGet
-    hookfunction(game.HttpGet, newcclosure(function(self, ...)
-        if self == game then
-            local url = select(1, ...)
-            if url == originalHttpGet then
-                while true do end
-                return nil
-            end
-        end
-        return originalHttpGet(self, ...)
-    end))
-end
-
- loadstring(game:HttpGet("https://raw.githubusercontent.com/lolnoobsloadstring/a78eft78v638qb476ba78w/refs/heads/main/loader/78637bcawubsg76c73/byp.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lolnoobsloadstring/a78eft78v638qb476ba78w/refs/heads/main/loader/78637bcawubsg76c73/byp.lua"))()
 
 local CommandsList = Instance.new("ScreenGui")
 local InnerFrame = Instance.new("CanvasGroup")
